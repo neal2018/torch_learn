922abda58ef17383afc18f7cfd8014eab1069f6d
neal <neal.lion@icloud.com>
2020-08-26 01:23:53 +0800

    save test change

--- test.py
+++ test.py
@@ -1,48 +1,108 @@
 import torch
+import torchvision
+import torchvision.transforms as transforms
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
+import matplotlib.pyplot as plt
+import numpy as np
+
+import os
 
 
 class Net(nn.Module):
     def __init__(self):
         super().__init__()
-        self.conv1 = nn.Conv2d(1, 6, 3)
-        self.conv2 = nn.Conv2d(6, 16, 3)
-        self.fc1 = nn.Linear(16*6*6, 120)
+        self.conv1 = nn.Conv2d(3, 12, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(12, 16, 5)
+        self.fc1 = nn.Linear(16*5*5, 120)
         self.fc2 = nn.Linear(120, 84)
         self.fc3 = nn.Linear(84, 10)
 
     def forward(self, x):
-        x = F.max_pool2d(F.relu(self.conv1(x)), (2, 2))
-        x = F.max_pool2d(F.relu(self.conv2(x)), 2)
-
-        x = x.view(-1, self.num_flat_features(x))
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = x.view(-1, 16*5*5)
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
         x = self.fc3(x)
         return x
 
-    def num_flat_features(self, x):
-        size = x.size()[1:]
-        num_features = 1
-        for s in size:
-            num_features *= s
-        return num_features
+
+def imshow(img):
+    img = img/2 + 0.5
+    npimg = img.numpy()
+    plt.imshow(np.transpose(npimg, (1, 2, 0)))
+    plt.show()
 
 
 if __name__ == "__main__":
-    net = Net()
-    params = net.parameters()
-    input_ = torch.randn(1, 1, 32, 32)
-    target = torch.randn(1, 10)
-    optimizer = optim.SGD(params, lr=0.01)
-    criterion = nn.MSELoss()
 
-    for i in range(100):
-        optimizer.zero_grad()
-        out = net(input_)
-        loss = criterion(target, out)
-        print(i, loss.item())
-        loss.backward()
-        optimizer.step()
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+    transform = transforms.Compose(
+        [transforms.ToTensor(),
+            transforms.Normalize((0.5,)*3, (0.5,)*3)]
+    )
+
+    trainset = torchvision.datasets.CIFAR10(
+        root="./data", train=True, download=True, transform=transform)
+
+    trainloader = torch.utils.data.DataLoader(
+        trainset, batch_size=4, shuffle=True, num_workers=15)
+
+    trainset = torchvision.datasets.CIFAR10(
+        root='./data', train=False,   download=True, transform=transform)
+
+    testloader = torch.utils.data.DataLoader(
+        trainset, batch_size=4, shuffle=False, num_workers=2)
+
+    classes = ('plane', 'car', 'bird', 'cat', 'deer',
+               'dog', 'frog', 'horse', 'ship', 'truck')
+
+    net = Net().to(device)
+
+    PATH = './cifar_net.pth'
+    if os.path.exists(PATH):
+        net.load_state_dict(torch.load(PATH))
+
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+
+    # training
+    for epoch in range(50):
+        running_loss = 0
+        for i, data in enumerate(trainloader, 0):
+            inputs, labels = data[0].to(device), data[1].to(device)
+
+            optimizer.zero_grad()
+            outputs = net(inputs)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+
+            running_loss += loss.item()
+            if i % 2000 == 1999:
+                print(f"[{epoch}, {i}] loss: {running_loss/2000}")
+                running_loss = 0
+        if epoch % 10 == 9:
+            torch.save(net.state_dict(), PATH)
+            print(f'{epoch=}, saved')
+
+    print("training done")
+
+    torch.save(net.state_dict(), PATH)
+
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        for data in testloader:
+            images, labels = data[0].to(device), data[1].to(device)
+            outputs = net(images)
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+
+    print('Accuracy of the network on the 10000 test images: %d %%' % (
+        100 * correct / total))
